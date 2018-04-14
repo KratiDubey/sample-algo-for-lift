a1fe70fef507c0f69d4e10bcba8d13967ae75c59
SOURAV2588 <souravghosh358@gmail.com>
2018-04-14 21:06:42 +0530

    Initial commit

---
+++ src/main/java/com/sourav/samplealgoforlift/SampleAlgoForLiftApplication.java
@@ -0,0 +1,12 @@
+package com.sourav.samplealgoforlift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleAlgoForLiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleAlgoForLiftApplication.class, args);
+	}
+}
